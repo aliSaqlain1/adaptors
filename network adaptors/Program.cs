22347c1ce20e1dbc66fc6963e87b87b606d948d1
aliSaqlain1 <57298196+aliSaqlain1@users.noreply.github.com>
2019-11-05 01:30:07 +0500

    new commit
    cs files

---
+++ network adaptors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+namespace network_adaptors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementScope wmiSrv;
+            ManagementObjectSearcher objSearcher;
+            ManagementObjectCollection objColl;
+            ObjectQuery objQuery;
+
+            ConnectionOptions connOpts = new ConnectionOptions();
+            objQuery = new ObjectQuery("SELECT * FROM Win32_NetworkAdapter WHERE AdapterTypeID <> NULL");
+            wmiSrv = new ManagementScope(@"root\cimv2", connOpts);
+            wmiSrv.Connect();
+            objSearcher = new ManagementObjectSearcher(wmiSrv, objQuery);
+            objSearcher.Options.Timeout = new TimeSpan(0, 0, 0, 0, 7000);
+            objColl = objSearcher.Get();
+            foreach (ManagementObject mo in objColl)
+            {
+                Console.WriteLine("Device ID: " + mo["DeviceID"].ToString());
+                Console.WriteLine("   Adapter type: " + mo["AdapterType"].ToString());
+                Console.WriteLine("   Description: " + mo["Description"].ToString());
+                Console.WriteLine("   MAC address: " + mo["MACAddress"].ToString());
+                Console.WriteLine("   Manufacturer: " + mo["Manufacturer"].ToString());
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
